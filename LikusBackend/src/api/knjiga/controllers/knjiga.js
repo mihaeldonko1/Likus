8c5a1b4007dc5d3af9855acfbc5ecaf7747c6405
mihad <miha.donko@gmail.com>
2023-05-26 15:57:12 +0200

    new schema for knjiga

---
+++ LikusBackend/src/api/knjiga/controllers/knjiga.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * knjiga controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::knjiga.knjiga');
