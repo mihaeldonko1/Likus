e1f7994fd37ff78919be167d7a23694e19516ce0
mihahorvat <miha.horvat5@student.um.si>
2023-05-28 21:03:12 +0200

    updatan frontend 1/3

--- Frontend/storage/framework/views/4e1746c551312a00ada03b3eba291e1efb6535fa.php
+++ Frontend/storage/framework/views/4e1746c551312a00ada03b3eba291e1efb6535fa.php
@@ -1,5 +1,5 @@
-@extends('layouts.app')
-@section('content')
+
+<?php $__env->startSection('content'); ?>
 
 <style>
     body {
@@ -70,51 +70,51 @@
     <div class="d-flex justify-content-center">
         <div class="pagination">
             <ul class="pagination">
-                <li class="page-item{{ ($members->currentPage() === 1) ? ' disabled' : '' }}">
-                    <a class="page-link" href="{{ $members->previousPageUrl() }}" aria-label="Previous">
+                <li class="page-item<?php echo e(($members->currentPage() === 1) ? ' disabled' : ''); ?>">
+                    <a class="page-link" href="<?php echo e($members->previousPageUrl()); ?>" aria-label="Previous">
                         <span aria-hidden="true">&laquo;</span>
                         <span class="sr-only">Previous</span>
                     </a>
                 </li>
 
-                @php
+                <?php
                     $currentPage = $members->currentPage();
                     $lastPage = $members->lastPage();
                     $range = 2; // Number of pages to show before and after the current page
                     $startPage = max($currentPage - $range, 1);
                     $endPage = min($currentPage + $range, $lastPage);
-                @endphp
+                ?>
 
-                @if ($startPage > 1)
+                <?php if($startPage > 1): ?>
                     <li class="page-item">
-                        <a class="page-link" href="{{ $members->url(1) }}">1</a>
+                        <a class="page-link" href="<?php echo e($members->url(1)); ?>">1</a>
                     </li>
-                    @if ($startPage > 2)
+                    <?php if($startPage > 2): ?>
                         <li class="page-item disabled">
                             <a class="page-link" href="#">...</a>
                         </li>
-                    @endif
-                @endif
+                    <?php endif; ?>
+                <?php endif; ?>
 
-                @foreach ($members->getUrlRange($startPage, $endPage) as $page => $url)
-                    <li class="page-item{{ ($members->currentPage() === $page) ? ' active' : '' }}">
-                        <a class="page-link" href="{{ $url }}">{{ $page }}</a>
+                <?php $__currentLoopData = $members->getUrlRange($startPage, $endPage); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $page => $url): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li class="page-item<?php echo e(($members->currentPage() === $page) ? ' active' : ''); ?>">
+                        <a class="page-link" href="<?php echo e($url); ?>"><?php echo e($page); ?></a>
                     </li>
-                @endforeach
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 
-                @if ($endPage < $lastPage)
-                    @if ($endPage < ($lastPage - 1))
+                <?php if($endPage < $lastPage): ?>
+                    <?php if($endPage < ($lastPage - 1)): ?>
                         <li class="page-item disabled">
                             <a class="page-link" href="#">...</a>
                         </li>
-                    @endif
+                    <?php endif; ?>
                     <li class="page-item">
-                        <a class="page-link" href="{{ $members->url($lastPage) }}">{{ $lastPage }}</a>
+                        <a class="page-link" href="<?php echo e($members->url($lastPage)); ?>"><?php echo e($lastPage); ?></a>
                     </li>
-                @endif
+                <?php endif; ?>
 
-                <li class="page-item{{ ($members->currentPage() === $members->lastPage()) ? ' disabled' : '' }}">
-                    <a class="page-link" href="{{ $members->nextPageUrl() }}" aria-label="Next">
+                <li class="page-item<?php echo e(($members->currentPage() === $members->lastPage()) ? ' disabled' : ''); ?>">
+                    <a class="page-link" href="<?php echo e($members->nextPageUrl()); ?>" aria-label="Next">
                         <span aria-hidden="true">&raquo;</span>
                         <span class="sr-only">Next</span>
                     </a>
@@ -128,24 +128,25 @@
 
     <div class="container">
         <div class="row">
-            @foreach ($members->items() as $item)
+            <?php $__currentLoopData = $members->items(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <div class="col-sm-6 col-md-3 col-lg-3 col-xl-3 mb-4">
                     <div class="card text-center">
-                        @if (isset($item['attributes']['Profilna_slika']['data'][0]['attributes']['url']))
-                            <img src="http://localhost:1337{{ $item['attributes']['Profilna_slika']['data'][0]['attributes']['url'] }}" class="card-img-top mx-auto d-block mt-3 card-img">
-                        @else
+                        <?php if(isset($item['attributes']['Profilna_slika']['data'][0]['attributes']['url'])): ?>
+                            <img src="http://localhost:1337<?php echo e($item['attributes']['Profilna_slika']['data'][0]['attributes']['url']); ?>" class="card-img-top mx-auto d-block mt-3 card-img">
+                        <?php else: ?>
                             <img src="https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-7.jpg" class="card-img-top mx-auto d-block mt-3 card-img">
-                        @endif
+                        <?php endif; ?>
                         <div class="card-body">
                             <h5 class="card-title">
-                                <a href="clan/{{ $item['id'] }}">
-                                    {{ $item['attributes']['Ime'] }} <br /> {{ $item['attributes']['Priimek'] }}
+                                <a href="clan/<?php echo e($item['id']); ?>">
+                                    <?php echo e($item['attributes']['Ime']); ?> <br /> <?php echo e($item['attributes']['Priimek']); ?>
+
                                 </a>
                             </h5>
                         </div>
                     </div>
                 </div>
-            @endforeach
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
         </div>
     </div>
 </div>
@@ -174,4 +175,6 @@
         }
     });
 </script>
-@endsection
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Miha\Desktop\Praktikum2\Likus\Frontend\resources\views/members.blade.php ENDPATH**/ ?>
