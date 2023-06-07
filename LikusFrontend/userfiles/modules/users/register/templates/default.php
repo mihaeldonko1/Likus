79a0e0618c9f80d1f5087ca9d9becd2e37853c64
JanVolovsek <jan.volovsek@student.um.si>
2023-06-08 00:52:30 +0200

    update

--- LikusFrontend/userfiles/modules/users/register/templates/default.php
+++ LikusFrontend/userfiles/modules/users/register/templates/default.php
@@ -38,24 +38,24 @@ description: Default register template
         <form class="p-t-10" action="#" id="user_registration_form_holder" method="post">
             <?php print csrf_form(); ?>
             <div class="form-group">
-                    <label class="control-label"><?php _e('Ime'); ?></label>
+                    <label class="control-label"><?php _e('Ime :'); ?></label>
                     <input class="form-control input-lg" type="text" name="first_name" placeholder="<?php _e('Ime'); ?>">
                 </div>
 
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Priimek'); ?></label>
+                    <label class="control-label"><?php _e('Priimek :'); ?></label>
                     <input class="form-control input-lg" type="text" name="last_name" placeholder="<?php _e('Priimek'); ?>">
                 </div>
 
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('E-mail'); ?></label>
+                    <label class="control-label"><?php _e('E-mail :'); ?></label>
                     <input class="form-control input-lg" type="email" name="email" placeholder="<?php _e('E-mail'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Izberite tip spola'); ?></label>
+                    <label class="control-label"><?php _e('Izberite tip spola :'); ?></label>
                     <div>
                         <br />
                         <input type="radio" id="option3" name="Spol" value="Moški">
@@ -69,32 +69,32 @@ description: Default register template
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Datum rojstva'); ?></label>
+                    <label class="control-label"><?php _e('Datum rojstva :'); ?></label>
                     <input class="form-control input-lg" type="date" name="Rojstni_dan" placeholder="<?php _e('Datum rojstva'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Telefon'); ?></label>
+                    <label class="control-label"><?php _e('Telefon :'); ?></label>
                     <input class="form-control input-lg" type="text" name="phone" placeholder="<?php _e('Telefon'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Naslov'); ?></label>
+                    <label class="control-label"><?php _e('Naslov :'); ?></label>
                     <input class="form-control input-lg" type="text" name="Naslov" placeholder="<?php _e('Naslov'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Pošta'); ?></label>
+                    <label class="control-label"><?php _e('Pošta :'); ?></label>
                     <input class="form-control input-lg" type="text" name="Posta" placeholder="<?php _e('Pošta'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Poštna številka'); ?></label>
+                    <label class="control-label"><?php _e('Poštna številka :'); ?></label>
                     <input class="form-control input-lg" type="text" name="Postna_stevilka" placeholder="<?php _e('Poštna številka'); ?>">
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Država'); ?></label>
+                    <label class="control-label"><?php _e('Država :'); ?></label>
                     <input class="form-control input-lg" type="text" name="Drzava" placeholder="<?php _e('Država'); ?>">
                 </div>
                 <div class="row">
@@ -112,13 +112,17 @@ description: Default register template
                         </div>
                         </div>
                     </div>
-                    <div class="col-md-6">
-                        <a href="/likus-in-slps" class="btn btn-primary">Preberi več</a>
-                    </div>
+                   
                 </div>
 
+                <div class="col-md-6">
+                        <a href="/likus-in-slps" class="btn btn-primary">Preberi več</a>
+                    </div>
+
+                    <br />
+
                 <div class="form-group">
-                    <label class="control-label"><?php _e('Geslo'); ?></label>
+                    <label class="control-label"><?php _e('Geslo :'); ?></label>
                     <input class="form-control input-lg" type="password" name="password" placeholder="<?php _e('Geslo'); ?>">
                 </div>
 
