d5ad2f371f0d7ce79e05c5e22913737c1e0c03e8
mihad <miha.donko@gmail.com>
2023-07-07 22:55:20 +0200

    Knjige update

--- LikusFrontend/resources/views/singlemember.blade.php
+++ LikusFrontend/resources/views/singlemember.blade.php
@@ -266,122 +266,119 @@ color: #e89443;
         </div>  
     @endif
     </div>
-                <br>
-                <br>
-                <br>
-                
-                
-
+        <br>
+        <br>
+        <br>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js"></script>
     <script type="text/javascript" src="../resources/js/extras/jquery.min.1.7.js"></script>
     <script type="text/javascript" src="../resources/js/extras/modernizr.2.5.3.min.js"></script>
-<script src="../resources/js/main.js"></script>
-<script src="../resources/js/bookifyPDF.min.js"></script>
-<script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
-<script src="https://kit.fontawesome.com/b0f29e9bfe.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script>
-$(document).ready(function() {
-    var defaultContent = $('.body-book').html();
+    <script src="../resources/js/main.js"></script>
+    <script src="../resources/js/bookifyPDF.min.js"></script>
+    <script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
+    <script src="https://kit.fontawesome.com/b0f29e9bfe.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+    $(document).ready(function() {
+        var defaultContent = $('.body-book').html();
 
-    $(".bookLoader").click(function() {
-    $('.container-bookify').html(defaultContent);
-    var bookValue = $(this).data("book");
-    fetch(`http://localhost:1337/api/clanki/${bookValue}?populate=*`)
-        .then(response => response.json())
-        .then(data => {
-            let uri = data.data.attributes.Clanek.data.attributes.url;
-             let fullurl = "http://localhost:1337"+uri;        
-            var flipBookWidth = 1080;
-            var flipBookHeight = 703;
-            const flipBookWidthFinal = 1080;
+        $(".bookLoader").click(function() {
+        $('.container-bookify').html(defaultContent);
+        var bookValue = $(this).data("book");
+        fetch(`http://localhost:1337/api/clanki/${bookValue}?populate=*`)
+            .then(response => response.json())
+            .then(data => {
+                let uri = data.data.attributes.Clanek.data.attributes.url;
+                let fullurl = "http://localhost:1337"+uri;        
+                var flipBookWidth = 1080;
+                var flipBookHeight = 703;
+                const flipBookWidthFinal = 1080;
 
-            if (window.innerWidth < 1000) {
-                if (window.innerWidth < flipBookWidthFinal / 2) {
-                    flipBookWidth = window.innerWidth;
-                } else {
-                    flipBookWidth = flipBookWidth / 2;
-                }
-            }
-
-            function renderPDF(url, canvasContainer, options) {
-                var options = options || { scale: 1 };
-
-                function renderPage(page) {
-                    var viewport = page.getViewport({ scale: 1 });
-                    var scale = Math.min(flipBookWidth / viewport.width, flipBookHeight / viewport.height);
-                    viewport = page.getViewport({ scale: scale });
-                    var canvas = document.createElement('canvas');
-                    var ctx = canvas.getContext('2d');
-                    var renderContext = {
-                        canvasContext: ctx,
-                        viewport: viewport
-                    };
-
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
-
-                    var pageDiv = document.createElement('div');
-                    pageDiv.appendChild(canvas);
-                    canvasContainer.append(pageDiv);
-
-                    return page.render(renderContext).promise;
-                }
-
-                function renderPages(pdfDoc) {
-                    var pages = [];
-                    for (var num = 1; num <= pdfDoc.numPages; num++) {
-                        pages.push(pdfDoc.getPage(num).then(renderPage));
+                if (window.innerWidth < 1000) {
+                    if (window.innerWidth < flipBookWidthFinal / 2) {
+                        flipBookWidth = window.innerWidth;
+                    } else {
+                        flipBookWidth = flipBookWidth / 2;
                     }
-                    return Promise.all(pages);
                 }
 
-                return pdfjsLib.getDocument(url).promise.then(renderPages);
-            }
+                function renderPDF(url, canvasContainer, options) {
+                    var options = options || { scale: 1 };
 
-            function loadApp() {
-                var display = window.innerWidth < 1000 ? 'single' : 'double';
-                $('.flipbook').turn({
-                    width: flipBookWidth,
-                    height: flipBookHeight,
-                    elevation: 50,
-                    gradients: true,
-                    display: display,
-                    autoCenter: true
-                });
-            }
+                    function renderPage(page) {
+                        var viewport = page.getViewport({ scale: 1 });
+                        var scale = Math.min(flipBookWidth / viewport.width, flipBookHeight / viewport.height);
+                        viewport = page.getViewport({ scale: scale });
+                        var canvas = document.createElement('canvas');
+                        var ctx = canvas.getContext('2d');
+                        var renderContext = {
+                            canvasContext: ctx,
+                            viewport: viewport
+                        };
 
-            yepnope({
-                test: Modernizr.csstransforms,
-                yep: ['../resources/js/lib/turn.js'],
-                nope: ['../resources/js/lib/turn.html4.min.js'],
-                both: ['css/basic.css'],
-                complete: function () {
-                    renderPDF(fullurl, $(".flipbook")).then(loadApp).then(function () {
-                        $('#next').click(function () {
-                            $('.flipbook').turn('next');
-                        });
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
 
-                        $('#prev').click(function () {
-                            $('.flipbook').turn('previous');
-                        });
+                        var pageDiv = document.createElement('div');
+                        pageDiv.appendChild(canvas);
+                        canvasContainer.append(pageDiv);
 
-                        $(document).keydown(function (e) {
-                            if (e.keyCode == 37) {
-                                $('.flipbook').turn('previous');
-                            } else if (e.keyCode == 39) {
-                                $('.flipbook').turn('next');
-                            }
-                        });
+                        return page.render(renderContext).promise;
+                    }
+
+                    function renderPages(pdfDoc) {
+                        var pages = [];
+                        for (var num = 1; num <= pdfDoc.numPages; num++) {
+                            pages.push(pdfDoc.getPage(num).then(renderPage));
+                        }
+                        return Promise.all(pages);
+                    }
+
+                    return pdfjsLib.getDocument(url).promise.then(renderPages);
+                }
+
+                function loadApp() {
+                    var display = window.innerWidth < 1000 ? 'single' : 'double';
+                    $('.flipbook').turn({
+                        width: flipBookWidth,
+                        height: flipBookHeight,
+                        elevation: 50,
+                        gradients: true,
+                        display: display,
+                        autoCenter: true
                     });
                 }
+
+                yepnope({
+                    test: Modernizr.csstransforms,
+                    yep: ['../resources/js/lib/turn.js'],
+                    nope: ['../resources/js/lib/turn.html4.min.js'],
+                    both: ['css/basic.css'],
+                    complete: function () {
+                        renderPDF(fullurl, $(".flipbook")).then(loadApp).then(function () {
+                            $('#next').click(function () {
+                                $('.flipbook').turn('next');
+                            });
+
+                            $('#prev').click(function () {
+                                $('.flipbook').turn('previous');
+                            });
+
+                            $(document).keydown(function (e) {
+                                if (e.keyCode == 37) {
+                                    $('.flipbook').turn('previous');
+                                } else if (e.keyCode == 39) {
+                                    $('.flipbook').turn('next');
+                                }
+                            });
+                        });
+                    }
+                });
+    //tu se konca book
+            })
+            .catch(error => {
+                console.error(error);
             });
-//tu se konca book
-        })
-        .catch(error => {
-            console.error(error);
         });
-    });
 
     let zivljenjepisId = "http://localhost:1337<?php echo $zivljenjepisId; ?>";
     if (zivljenjepisId!=0) {
